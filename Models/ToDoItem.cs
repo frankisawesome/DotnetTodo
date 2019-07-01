a5f21c0427cab4fcaffa15eba9416a01a99f038b
Frank Li <b439988l@gmail.com>
2019-07-01 17:32:14 +0800

    init"
    "

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+namespace ToDoAPI.Models
+{
+    public class ToDoItem 
+    {
+        public long Id { get; set;}
+
+        public string Name { get; set;}
+
+        public bool IsCompelete { get; set;}
+    }
+}
